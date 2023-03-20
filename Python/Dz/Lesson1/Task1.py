63c56a4a09cd005161e923388e84ce37f467db8c
TimurGadisov <gadisovtmr@gmail.com>
2023-03-20 20:13:00 +0300

    1 и 2 задание дз

---
+++ Python/Dz/Lesson1/Task1.py
@@ -0,0 +1,17 @@
+# Задача 2
+# n = int(input(print("Введите трехзначное число")))
+# summa = 0
+# while n > 10:
+#     summa = summa + (n % 10)
+#     n = n // 10
+# print('сумма цифр равна', summa )
+
+# Задача 4
+
+
+n = int(input(print("Сколько всего журавликов?")))
+x1 = n//3
+x2 =x1*2
+x1 = x1//2
+print(x1, x2, x1)
+
