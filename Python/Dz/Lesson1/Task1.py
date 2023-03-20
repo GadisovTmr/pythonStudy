10d4c5618b478df78c2813bc80d4262c12d7dadb
TimurGadisov <gadisovtmr@gmail.com>
2023-03-20 20:13:00 +0300

    1 и 2 задание дз

---
+++ Python/Dz/Lesson1/Task1.py
@@ -0,0 +1,32 @@
+# Задача 2
+# def sumofthree(n):
+#     summa = 0
+#     while n > 0:
+#         summa = summa + (n % 10)
+#         n = n // 10
+#     return summa
+# print(sumofthree(345))
+
+# Задача 4
+# n = int(input(print("Сколько всего журавликов?")))
+# x1 = n//6
+# x2 =x1*4
+# print(x1, x2, x1)
+
+# Задача  6
+# n = int(input(print("Какой номер билета?")))
+# def sumofthree(n):
+#     summa = 0
+#     while n > 0:
+#         summa = summa + (n % 10)
+#         n = n // 10
+#     return summa
+#
+# firstpart = n%1000
+# secondpart = n//1000
+#
+# if sumofthree(firstpart) == sumofthree(secondpart):
+#     print('yes')
+# else:
+#     print('no')
+
